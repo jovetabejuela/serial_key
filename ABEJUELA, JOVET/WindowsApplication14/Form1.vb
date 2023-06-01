521e95e25b4bfba7bc31554850c82d7488e0fa2a
Jovetabejuela <135179830+jovetabejuela@users.noreply.github.com>
2023-06-01 19:06:35 +0800

    Add files via upload

---
+++ ABEJUELA, JOVET/WindowsApplication14/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.LoadCount < 0 Then
+            Form2.ShowDialog()
+
+        End If
+
+        Me.Text = My.Settings.Appstat & " : Count = >" &
+        My.Settings.LoadCount
+        My.Settings.LoadCount -= 1
+        My.Settings.Save()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox(" ABEJUELA,JOVET M.")
+
+    End Sub
+End Class
---
+++ ABEJUELA, JOVET/WindowsApplication14/Form2.vb
@@ -0,0 +1,24 @@
+﻿Imports System.ComponentModel
+
+Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '121198
+        If My.Settings.Serialkey = TextBox1.Text Then
+            My.Settings.Appstat = " Full Version"
+            My.Settings.Save()
+            Close()
+        Else
+            MsgBox(" Invalid Serial Key")
+        End If
+
+    End Sub
+
+    Private Sub Form2_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        End
+
+    End Sub
+End Class
